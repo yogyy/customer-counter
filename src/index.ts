7161d2f12d8e26dd5225c793aaf1ab549846930d
yogyy <yogimr261@gmail.com>
2025-07-23 23:19:55 +0700

    feat: add key-value store

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,36 @@ import { eq, sql } from "drizzle-orm";
 import { zValidator } from "@hono/zod-validator";
 import z4 from "zod/v4";
 import { cors } from "hono/cors";
+import { Redis } from "@upstash/redis/cloudflare";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
-const customerId = "blablabla";
+const customerId = "F8pXzR7t-Q2nWJvBcY_5";
+const cacheKey = "customer:counter";
 
 app.use("/api/*", cors({ origin: "https://binar-binar.pages.dev" }));
 
 app
   .get("/", (c) => c.text("constantine"))
+
   .basePath("/api")
   .get("/update", async (c) => {
-    await createDB(c.env)
-      .update(customer)
-      .set({ total: sql`${customer.total} + 1` })
-      .where(eq(customer.id, customerId));
+    try {
+      const [data] = await createDB(c.env)
+        .update(customer)
+        .set({ total: sql`${customer.total} + 1` })
+        .where(eq(customer.id, customerId))
+        .returning();
 
-    return c.json({ message: "total updated." });
+      const redis = Redis.fromEnv(c.env);
+      const cached = await redis.hgetall(cacheKey);
+
+      if (cached) await redis.hset(cacheKey, data);
+
+      return c.json({ message: "total updated." });
+    } catch (err) {
+      console.error(err);
+      return c.json({ error: "Internal server error" }, 500);
+    }
   })
   .post(
     "/update",
@@ -28,18 +42,49 @@ app
     async (c) => {
       const { total } = c.req.valid("json");
 
-      await createDB(c.env)
-        .update(customer)
-        .set({ total })
-        .where(eq(customer.id, customerId));
-      return c.json({ message: "total updated!" });
+      try {
+        const [data] = await createDB(c.env)
+          .update(customer)
+          .set({ total })
+          .where(eq(customer.id, customerId))
+          .returning();
+
+        const redis = Redis.fromEnv(c.env);
+        const cached = await redis.hgetall(cacheKey);
+
+        if (cached) await redis.hset(cacheKey, data);
+
+        return c.json({ message: "total updated!" });
+      } catch (err) {
+        console.error(err);
+        return c.json({ error: "Internal server error" }, 500);
+      }
     }
   )
   .get("/total", async (c) => {
-    const result = await createDB(c.env).query.customer.findFirst({
-      where: eq(customer.id, customerId),
-    });
-    return c.json(result);
+    const redis = Redis.fromEnv(c.env);
+
+    try {
+      const cached = await redis.hgetall(cacheKey);
+      if (cached) return c.json(cached);
+
+      const result = await createDB(c.env).query.customer.findFirst({
+        where: eq(customer.id, customerId),
+      });
+
+      if (result) {
+        const p = redis.pipeline();
+        p.hset(cacheKey, result);
+        p.expire(cacheKey, 300);
+
+        await p.exec();
+      }
+
+      return c.json(result);
+    } catch (err) {
+      console.error(err);
+      return c.json({ error: "Internal server error" }, 500);
+    }
   });
 
 export default app;
