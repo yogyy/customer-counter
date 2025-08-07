5c209758d532b9af129599d05b5e2e16bf8171c8
yogyy <yogimr261@gmail.com>
2025-08-07 23:57:39 +0700

    chore: update post route

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,76 @@
 import { Hono } from "hono";
 import { createDB } from "./db";
 import { customer } from "./db/schema";
+import type { Session, User } from "./db/schema";
 import { eq, sql } from "drizzle-orm";
 import { zValidator } from "@hono/zod-validator";
 import z4 from "zod/v4";
 import { cors } from "hono/cors";
 import { Redis } from "@upstash/redis/cloudflare";
+import { authMiddleware, authRouter } from "./lib/auth";
 
-const app = new Hono<{ Bindings: CloudflareBindings }>();
-const customerId = "F8pXzR7t-Q2nWJvBcY_5";
+const app = new Hono<{
+  Bindings: CloudflareBindings;
+  Variables: {
+    user: User;
+    session: Session;
+  };
+}>();
+const customerId = "yogg_F8pXzR7t-Q2nWJvBcY_5";
 const cacheKey = "customer:counter";
 
-app.use("/api/*", cors({ origin: "https://binar-binar.pages.dev" }));
+app
+  .use(authMiddleware)
+  .use(
+    "/api/*",
+    cors({ origin: ["https://binar-binar.pages.dev", "http://localhost:5173"] })
+  );
 
 app
-  .get("/", (c) => c.text("constantine"))
+  .get("/", (c) => {
+    const user = c.get("user");
 
+    if (!user) {
+      return c.text("constantine");
+    }
+    return c.text(user.name);
+  })
+
+  .route("/", authRouter)
   .basePath("/api")
+  .get("/total", async (c) => {
+    const redis = Redis.fromEnv(c.env);
+
+    try {
+      const cached = await redis.hgetall(cacheKey);
+      if (cached) return c.json(cached);
+
+      const result = await createDB(c.env).query.customer.findFirst({
+        where: eq(customer.id, customerId),
+      });
+
+      if (result) {
+        const p = redis.pipeline();
+        p.hset(cacheKey, result);
+        p.expire(cacheKey, 300);
+
+        await p.exec();
+      }
+
+      return c.json(result);
+    } catch (err) {
+      console.error(err);
+      return c.json({ error: "Internal server error" }, 500);
+    }
+  })
+  .use(async (c, next) => {
+    const user = c.get("user");
+    if (!user?.isAdmin) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
+    await next();
+  })
   .get("/update", async (c) => {
     try {
       const [data] = await createDB(c.env)
@@ -60,31 +114,6 @@ app
         return c.json({ error: "Internal server error" }, 500);
       }
     }
-  )
-  .get("/total", async (c) => {
-    const redis = Redis.fromEnv(c.env);
-
-    try {
-      const cached = await redis.hgetall(cacheKey);
-      if (cached) return c.json(cached);
-
-      const result = await createDB(c.env).query.customer.findFirst({
-        where: eq(customer.id, customerId),
-      });
-
-      if (result) {
-        const p = redis.pipeline();
-        p.hset(cacheKey, result);
-        p.expire(cacheKey, 300);
-
-        await p.exec();
-      }
-
-      return c.json(result);
-    } catch (err) {
-      console.error(err);
-      return c.json({ error: "Internal server error" }, 500);
-    }
-  });
+  );
 
 export default app;
